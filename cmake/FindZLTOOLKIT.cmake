556552d2246810c61f7640f7ee8710eaad1cf927
xiongziliang <771730766@qq.com>
2017-10-13 15:23:37 +0800

    修改cmake脚本

--- cmake/FindZLTOOLKIT.cmake
+++ cmake/FindZLTOOLKIT.cmake
@@ -1,10 +1,18 @@
 find_path(ZLTOOLKIT_INCLUDE_DIR
-  NAMES ZLToolKit/Network/Socket.h
-  PATHS $ENV{HOME}/ZLToolKit/include)
+  NAMES Network/Socket.h
+  PATHS 
+  ${PROJECT_SOURCE_DIR}/../ZLToolKit/build/include
+  $ENV{HOME}/ZLToolKit/include/ZLToolKit)
+
+message(STATUS "${ZLTOOLKIT_INCLUDE_DIR}")
 
 find_library(ZLTOOLKIT_LIBRARY
   NAMES ZLToolKit
-  PATHS $ENV{HOME}/ZLToolKit/lib)
+  PATHS 
+  ${PROJECT_SOURCE_DIR}/../ZLToolKit/build/lib
+  $ENV{HOME}/ZLToolKit/lib)
+
+message(STATUS "${ZLTOOLKIT_LIBRARY}")
 
 set(ZLTOOLKIT_LIBRARIES ${ZLTOOLKIT_LIBRARY})
 set(ZLTOOLKIT_INCLUDE_DIRS ${ZLTOOLKIT_INCLUDE_DIR})
