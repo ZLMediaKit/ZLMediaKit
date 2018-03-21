e087d45f9c21f31038deddd299fdc248e2d340c9
xiongziliang <771730766@qq.com>
2018-03-21 15:23:05 +0800

    更新编译脚本

--- build_for_ios.sh
+++ build_for_ios.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+path=`pwd`
+cd ..
+git clone --depth=1 https://github.com/xiongziliang/ZLMediaKit.git
+cd $path
+cd ZLMediaKit
+git submodule init
+git submodule update
 
 mkdir -p ios_build
 rm -rf ./build
@@ -6,4 +13,3 @@ ln -s ./ios_build build
 cd ios_build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.cmake -DIOS_PLATFORM=OS
 make -j4
-sudo make install
--- build_for_mac.sh
+++ build_for_mac.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 path=`pwd`
-wget https://raw.githubusercontent.com/xiongziliang/ZLToolKit/develop/build_for_mac.sh -O toolkit_build.sh
-sudo chmod +x ./toolkit_build.sh
-./toolkit_build.sh
+cd ..
+git clone --depth=1 https://github.com/xiongziliang/ZLMediaKit.git
+cd $path
+cd ZLMediaKit
+git submodule init
+git submodule update
+
+brew install cmake
+brew install mysql
+brew install openssl
 brew install x264
 brew install faac
 brew install mp4v2
 brew install sdl
 brew install ffmpeg
-cd $path
-cd ..
-git clone --depth=50 https://github.com/xiongziliang/ZLMediaKit.git
-cd ZLMediaKit
+
 mkdir -p mac_build
 rm -rf ./build
 ln -s ./mac_build build
 cd mac_build
 cmake .. -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2j/
 make -j4
-sudo make install
