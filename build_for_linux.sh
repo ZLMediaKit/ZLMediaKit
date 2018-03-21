0d620d25ce001fe4bff3d9e45db9343339aeffa3
xiongziliang <771730766@qq.com>
2018-03-21 15:23:05 +0800

    更新编译脚本

--- build_for_linux.sh
+++ build_for_linux.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 path=`pwd`
-wget https://raw.githubusercontent.com/xiongziliang/ZLToolKit/develop/build_for_linux.sh -O ./toolkit_build.sh
-sudo chmod +x ./toolkit_build.sh
-./toolkit_build.sh
+cd ..
+git clone --depth=1 https://github.com/xiongziliang/ZLMediaKit.git
+cd $path
+cd ZLMediaKit
+git submodule init
+git submodule update
+
+sudo apt-get install cmake
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install libssl-dev
 sudo apt-get install libx264-dev
 sudo apt-get install libfaac-dev
 sudo apt-get install libmp4v2-dev
 #sudo apt-get install libsdl-dev
 #sudo apt-get install libavcodec-dev
 #sudo apt-get install libavutil-dev
-cd $path
-cd ..
-git clone --depth=50 https://github.com/xiongziliang/ZLMediaKit.git
-cd ZLMediaKit
+
 mkdir -p linux_build
 rm -rf ./build
 ln -s ./linux_build build
@@ -20,4 +24,3 @@ cd linux_build
 
 cmake ..
 make -j4
-sudo make install
