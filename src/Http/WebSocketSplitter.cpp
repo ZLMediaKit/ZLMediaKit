6e0e734b96e41da6c8ed41d142f5df8e918d3ccc
xiongziliang <771730766@qq.com>
2018-09-25 09:55:41 +0800

    添加版权信息以及命名空间

--- src/Http/WebSocketSplitter.cpp
+++ src/Http/WebSocketSplitter.cpp
@@ -1,6 +1,28 @@
-﻿//
-// Created by xzl on 2018/9/21.
-//
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2016 xiongziliang <771730766@qq.com>
+ *
+ * This file is part of ZLMediaKit(https://github.com/xiongziliang/ZLMediaKit).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include "WebSocketSplitter.h"
 #include <sys/types.h>
@@ -9,6 +31,11 @@
 #include "Util/logger.h"
 #include "Util/util.h"
 using namespace ZL::Util;
+
+
+namespace ZL {
+namespace Http {
+
 /**
  *
   0             1                 2               3
@@ -176,6 +203,8 @@ void WebSocketSplitter::encode(uint8_t *data, uint64_t len) {
 
 
 
+} /* namespace Http */
+} /* namespace ZL */
 
 
 
