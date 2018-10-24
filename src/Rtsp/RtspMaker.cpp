f3f7a96281b223fd9cbed36a6e5c4be81492a27c
xiongziliang <771730766@qq.com>
2018-10-24 09:45:57 +0800

    添加RtspMaker类

---
+++ src/Rtsp/RtspMaker.cpp
@@ -0,0 +1,23 @@
+//
+// Created by xzl on 2018/10/24.
+//
+
+#include "RtspMaker.h"
+#include "Common/Factory.h"
+
+namespace ZL {
+namespace Rtsp {
+
+void RtspMaker::addTrack(const Track::Ptr &track, uint32_t ssrc, int mtu) {
+    if (track->getCodecId() == CodecInvalid) {
+        addTrack(std::make_shared<TitleSdp>(), ssrc, mtu);
+    } else {
+        Sdp::Ptr sdp = Factory::getSdpByTrack(track);
+        if (sdp) {
+            addTrack(sdp, ssrc, mtu);
+        }
+    }
+}
+
+} /* namespace Rtsp */
+} /* namespace ZL */
