2b9245b320ab0519c575e43442f635c6f29c6815
xiongziliang <771730766@qq.com>
2018-10-31 11:05:26 +0800

    修复编译bug

--- src/Rtsp/RtspSession.cpp
+++ src/Rtsp/RtspSession.cpp
@@ -973,7 +973,7 @@ void RtspSession::findStream(const function<void(bool)> &cb) {
 
 	NoticeCenter::Instance().addListener(task_id, Broadcast::kBroadcastMediaChanged, onRegist);
 	//5秒后执行失败回调
-	doDelay(5, [cb]() {
+	doDelay(5, [cb,task_id]() {
 		NoticeCenter::Instance().delListener(task_id,Broadcast::kBroadcastMediaChanged);
 		cb(false);
 	});
