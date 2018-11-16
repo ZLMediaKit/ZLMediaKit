214cc4c505cbd72bb8229e592203be728eaf18ef
xiongziliang <771730766@qq.com>
2018-11-16 15:46:49 +0800

    修复有些rtmp包FFmpeg无法解码的问题

--- src/RtmpMuxer/H264RtmpCodec.cpp
+++ src/RtmpMuxer/H264RtmpCodec.cpp
@@ -153,28 +153,33 @@ void H264RtmpEncoder::inputFrame(const Frame::Ptr &frame) {
     switch (type){
         case H264Frame::NAL_IDR:
         case H264Frame::NAL_B_P:{
-            //I or P or B frame
-            int8_t flags = 7; //h.264
-            bool is_config = false;
-            bool keyFrame = frame->keyFrame();
-            flags |= ((keyFrame ? FLV_KEY_FRAME : FLV_INTER_FRAME) << 4);
+            if(_lastPacket && _lastPacket->timeStamp != frame->stamp()) {
+                RtmpCodec::inputRtmp(_lastPacket, _lastPacket->isVideoKeyFrame());
+                _lastPacket = nullptr;
+            }
 
-            RtmpPacket::Ptr rtmpPkt = ResourcePoolHelper<RtmpPacket>::obtainObj();
-            rtmpPkt->strBuf.clear();
+            if(!_lastPacket) {
+                //I or P or B frame
+                int8_t flags = 7; //h.264
+                bool is_config = false;
+                flags |= ((frame->keyFrame() ? FLV_KEY_FRAME : FLV_INTER_FRAME) << 4);
 
-            rtmpPkt->strBuf.push_back(flags);
-            rtmpPkt->strBuf.push_back(!is_config);
-            rtmpPkt->strBuf.append("\x0\x0\x0", 3);
+                _lastPacket = ResourcePoolHelper<RtmpPacket>::obtainObj();
+                _lastPacket->strBuf.clear();
+                _lastPacket->strBuf.push_back(flags);
+                _lastPacket->strBuf.push_back(!is_config);
+                _lastPacket->strBuf.append("\x0\x0\x0", 3);
+
+                _lastPacket->chunkId = CHUNK_VIDEO;
+                _lastPacket->streamId = STREAM_MEDIA;
+                _lastPacket->timeStamp = frame->stamp();
+                _lastPacket->typeId = MSG_VIDEO;
+
+            }
             auto size = htonl(iLen);
-            rtmpPkt->strBuf.append((char *) &size, 4);
-            rtmpPkt->strBuf.append(pcData, iLen);
-
-            rtmpPkt->bodySize = rtmpPkt->strBuf.size();
-            rtmpPkt->chunkId = CHUNK_VIDEO;
-            rtmpPkt->streamId = STREAM_MEDIA;
-            rtmpPkt->timeStamp = frame->stamp();
-            rtmpPkt->typeId = MSG_VIDEO;
-            RtmpCodec::inputRtmp(rtmpPkt,keyFrame);
+            _lastPacket->strBuf.append((char *) &size, 4);
+            _lastPacket->strBuf.append(pcData, iLen);
+            _lastPacket->bodySize = _lastPacket->strBuf.size();
         }
             break;
 
