4560c3b1462246d67594b526e7e0949649c9fd6e
xiongziliang <771730766@qq.com>
2019-05-29 09:28:35 +0800

    优化虚拟主机相关

--- src/Common/MediaSource.cpp
+++ src/Common/MediaSource.cpp
@@ -235,19 +235,16 @@ void MediaInfo::parse(const string &url){
     if(_vhost.empty()){
         //无效vhost
         _vhost = DEFAULT_VHOST;
-    }else{
-        if(INADDR_NONE != inet_addr(_vhost.data())){
-            //这是ip,未指定vhost;使用默认vhost
-            _vhost = DEFAULT_VHOST;
-        }else if(_vhost == "localhost"){
-            //localhost也是默认vhost
-            _vhost = DEFAULT_VHOST;
-        }else if(!enableVhost){
-            //关闭了虚拟主机
-            _vhost = DEFAULT_VHOST;
-        }
+    }else if(INADDR_NONE != inet_addr(_vhost.data())){
+        //这是ip,未指定vhost;使用默认vhost
+        _vhost = DEFAULT_VHOST;
+    }else if(_vhost == "localhost"){
+        //localhost也是默认vhost
+        _vhost = DEFAULT_VHOST;
+    }else if(!enableVhost){
+        //关闭了虚拟主机
+        _vhost = DEFAULT_VHOST;
     }
-
 }
 
 
