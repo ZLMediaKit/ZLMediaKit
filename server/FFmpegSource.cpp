c055ba61350fafd65e8f2078a3519eebd93c956d
xiongziliang <771730766@qq.com>
2019-06-11 09:25:54 +0800

    添加协议头

--- server/FFmpegSource.cpp
+++ server/FFmpegSource.cpp
@@ -1,6 +1,28 @@
-//
-// Created by xzl on 2018/5/24.
-//
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2019 xiongziliang <771730766@qq.com>
+ *
+ * This file is part of ZLMediaKit(https://github.com/xiongziliang/ZLMediaKit).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include "FFmpegSource.h"
 #include "Common/config.h"
@@ -32,9 +54,9 @@ FFmpegSource::~FFmpegSource() {
 
 
 void FFmpegSource::play(const string &src_url,const string &dst_url,int timeout_ms,const onPlay &cb) {
-    GET_CONFIG_AND_REGISTER(string,ffmpeg_bin,FFmpeg::kBin);
-    GET_CONFIG_AND_REGISTER(string,ffmpeg_cmd,FFmpeg::kCmd);
-    GET_CONFIG_AND_REGISTER(string,ffmpeg_log,FFmpeg::kLog);
+    GET_CONFIG(string,ffmpeg_bin,FFmpeg::kBin);
+    GET_CONFIG(string,ffmpeg_cmd,FFmpeg::kCmd);
+    GET_CONFIG(string,ffmpeg_log,FFmpeg::kLog);
 
     _src_url = src_url;
     _dst_url = dst_url;
