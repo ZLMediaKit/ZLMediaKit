116cce58cdaf41d302e4393fc4dec2f3f6794d92
xiongziliang <771730766@qq.com>
2019-10-08 09:44:08 +0800

    提升sdp解析兼容性

--- src/Extension/Factory.cpp
+++ src/Extension/Factory.cpp
@@ -61,7 +61,7 @@ Track::Ptr Factory::getTrackBySdp(const SdpTrack::Ptr &track) {
 
     if (strcasecmp(track->_codec.data(), "h264") == 0) {
         //a=fmtp:96 packetization-mode=1;profile-level-id=42C01F;sprop-parameter-sets=Z0LAH9oBQBboQAAAAwBAAAAPI8YMqA==,aM48gA==
-        auto map = Parser::parseArgs(track->_fmtp,";","=");
+        auto map = Parser::parseArgs(FindField(track->_fmtp.data()," ", nullptr),";","=");
         for(auto &pr : map){
             trim(pr.second);
         }
@@ -78,7 +78,7 @@ Track::Ptr Factory::getTrackBySdp(const SdpTrack::Ptr &track) {
 
     if (strcasecmp(track->_codec.data(), "h265") == 0) {
         //a=fmtp:96 sprop-sps=QgEBAWAAAAMAsAAAAwAAAwBdoAKAgC0WNrkky/AIAAADAAgAAAMBlQg=; sprop-pps=RAHA8vA8kAA=
-        auto map = Parser::parseArgs(track->_fmtp,";","=");
+        auto map = Parser::parseArgs(FindField(track->_fmtp.data()," ", nullptr),";","=");
         for(auto &pr : map){
             trim(pr.second);
         }
