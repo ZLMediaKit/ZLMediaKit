4fb105d2566c28ecb5add5b3691d681612817750
xiongziliang <771730766@qq.com>
2019-11-01 15:40:21 +0800

    添加H265相关接口

--- src/Common/Device.cpp
+++ src/Common/Device.cpp
@@ -32,6 +32,7 @@
 #include "Util/TimeTicker.h"
 #include "Extension/AAC.h"
 #include "Extension/H264.h"
+#include "Extension/H265.h"
 
 using namespace toolkit;
 
@@ -106,6 +107,24 @@ void DevChannel::inputH264(const char* pcData, int iDataLen, uint32_t dts,uint32
     inputFrame(std::make_shared<H264FrameNoCacheAble>((char *)pcData,iDataLen,dts,pts,prefixeSize));
 }
 
+void DevChannel::inputH265(const char* pcData, int iDataLen, uint32_t dts,uint32_t pts) {
+	if(dts == 0){
+		dts = (uint32_t)_aTicker[0].elapsedTime();
+	}
+	if(pts == 0){
+		pts = dts;
+	}
+	int prefixeSize;
+	if (memcmp("\x00\x00\x00\x01", pcData, 4) == 0) {
+		prefixeSize = 4;
+	} else if (memcmp("\x00\x00\x01", pcData, 3) == 0) {
+		prefixeSize = 3;
+	} else {
+		prefixeSize = 0;
+	}
+	inputFrame(std::make_shared<H265FrameNoCacheAble>((char *)pcData,iDataLen,dts,pts,prefixeSize));
+}
+
 void DevChannel::inputAAC(const char* pcData, int iDataLen, uint32_t uiStamp,bool withAdtsHeader) {
 	if(withAdtsHeader){
 		inputAAC(pcData+7,iDataLen-7,uiStamp,pcData);
@@ -133,6 +152,11 @@ void DevChannel::inputAAC(const char *pcDataWithoutAdts,int iDataLen, uint32_t u
 void DevChannel::initVideo(const VideoInfo& info) {
 	_video = std::make_shared<VideoInfo>(info);
 	addTrack(std::make_shared<H264Track>());
+}
+
+void DevChannel::initH265Video(const VideoInfo &info){
+	_video = std::make_shared<VideoInfo>(info);
+	addTrack(std::make_shared<H265Track>());
 }
 
 void DevChannel::initAudio(const AudioInfo& info) {
