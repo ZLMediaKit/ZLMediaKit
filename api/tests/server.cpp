c72dd5b4c539735b5ff203f870788e846b9905d7
xiongziliang <771730766@qq.com>
2019-12-23 14:31:02 +0800

    通过windows测试

--- api/tests/server.cpp
+++ api/tests/server.cpp
@@ -1,10 +1,36 @@
-//
-// Created by xzl on 2019/12/23.
-//
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2019 xiongziliang <771730766@qq.com>
+ *
+ * This file is part of ZLMediaKit(https://github.com/xiongziliang/ZLMediaKit).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include <csignal>
 #include "mediakit.h"
+#ifdef _WIN32
+#include "windows.h"
+#else
 #include "unistd.h"
+#endif
 int main(int argc,char *argv[]){
     mk_env_init1(0,0,0, nullptr,0, nullptr, nullptr);
     mk_http_server_start(80,false);
@@ -14,7 +40,11 @@ int main(int argc,char *argv[]){
     static bool flag = true;
     signal(SIGINT, [](int) { flag = false; });// 设置退出信号
     while (flag){
+#ifdef _WIN32
+        Sleep(1000);
+#else
         sleep(1);
+#endif
     }
     mk_stop_all_server();
     return 0;
