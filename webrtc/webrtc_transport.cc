b0a1440ad7ba3b53fbc319c834b8d31b7d67fc33
ziyue <1213642868@qq.com>
2021-03-26 11:07:03 +0800

    完善

--- webrtc/webrtc_transport.cc
+++ webrtc/webrtc_transport.cc
@@ -4,31 +4,81 @@
 
 WebRtcTransport::WebRtcTransport() {
     static onceToken token([](){
-        Utils::Crypto::ClassInit();
         RTC::DtlsTransport::ClassInit();
         RTC::DepLibSRTP::ClassInit();
         RTC::SrtpSession::ClassInit();
     });
 
-    ice_server_ = std::make_shared<IceServer>(Utils::Crypto::GetRandomString(4), Utils::Crypto::GetRandomString(24));
-    ice_server_->SetIceServerCompletedCB([this]() {
-        this->OnIceServerCompleted();
-    });
-    ice_server_->SetSendCB([this](char *buf, size_t len, struct sockaddr_in *remote_address) {
-        this->WritePacket(buf, len, remote_address);
-    });
-
-    // todo dtls服务器或客户端模式
-    dtls_transport_ = std::make_shared<DtlsTransport>(true);
-    dtls_transport_->SetHandshakeCompletedCB([this](std::string client_key, std::string server_key, RTC::CryptoSuite srtp_crypto_suite) {
-        this->OnDtlsCompleted(client_key, server_key, srtp_crypto_suite);
-    });
-    dtls_transport_->SetOutPutCB([this](char *buf, size_t len) { this->WritePacket(buf, len); });
+    dtls_transport_ = std::make_shared<RTC::DtlsTransport>(EventPollerPool::Instance().getFirstPoller(), this);
+    ice_server_ = std::make_shared<RTC::IceServer>(this, makeRandStr(4), makeRandStr(24));
 }
 
 WebRtcTransport::~WebRtcTransport() {}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void WebRtcTransport::OnIceServerSendStunPacket(const RTC::IceServer *iceServer, const RTC::StunPacket *packet, RTC::TransportTuple *tuple) {
+    onWrite((char *)packet->GetData(), packet->GetSize(), (struct sockaddr_in *)tuple);
+}
+
+void WebRtcTransport::OnIceServerSelectedTuple(const RTC::IceServer *iceServer, RTC::TransportTuple *tuple) {
+    InfoL;
+}
+
+void WebRtcTransport::OnIceServerConnected(const RTC::IceServer *iceServer) {
+    InfoL;
+    dtls_transport_->Run(RTC::DtlsTransport::Role::SERVER);
+}
+
+void WebRtcTransport::OnIceServerCompleted(const RTC::IceServer *iceServer) {
+    InfoL;
+}
+
+void WebRtcTransport::OnIceServerDisconnected(const RTC::IceServer *iceServer) {
+    InfoL;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void WebRtcTransport::OnDtlsTransportConnected(
+        const RTC::DtlsTransport *dtlsTransport,
+        RTC::SrtpSession::CryptoSuite srtpCryptoSuite,
+        uint8_t *srtpLocalKey,
+        size_t srtpLocalKeyLen,
+        uint8_t *srtpRemoteKey,
+        size_t srtpRemoteKeyLen,
+        std::string &remoteCert) {
+    InfoL;
+    srtp_session_ = std::make_shared<RTC::SrtpSession>(RTC::SrtpSession::Type::OUTBOUND, srtpCryptoSuite, srtpLocalKey, srtpLocalKeyLen);
+    onDtlsConnected();
+}
+
+void WebRtcTransport::OnDtlsTransportSendData(const RTC::DtlsTransport *dtlsTransport, const uint8_t *data, size_t len) {
+    onWrite((char *)data, len);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void WebRtcTransport::onWrite(const char *buf, size_t len){
+    auto tuple = ice_server_->GetSelectedTuple();
+    assert(tuple);
+    onWrite(buf, len, (struct sockaddr_in *)tuple);
+}
+
 std::string WebRtcTransport::GetLocalSdp() {
+    RTC::DtlsTransport::Fingerprint remote_fingerprint;
+    remote_fingerprint.algorithm = RTC::DtlsTransport::GetFingerprintAlgorithm("sha-256");
+    remote_fingerprint.value = "";
+    dtls_transport_->SetRemoteFingerprint(remote_fingerprint);
+
+    string finger_print_sha256;
+    auto finger_prints = dtls_transport_->GetLocalFingerprints();
+    for (size_t i = 0; i < finger_prints.size(); i++) {
+        if (finger_prints[i].algorithm == RTC::DtlsTransport::FingerprintAlgorithm::SHA256) {
+            finger_print_sha256 = finger_prints[i].value;
+        }
+    }
+
     char sdp[1024 * 10] = {0};
     auto ssrc = getSSRC();
     auto ip = getIP();
@@ -60,20 +110,8 @@ std::string WebRtcTransport::GetLocalSdp() {
             "a=candidate:%s 1 udp %u %s %u typ %s\r\n",
             ip.c_str(), port, pt, ip.c_str(),
             ice_server_->GetUsernameFragment().c_str(),ice_server_->GetPassword().c_str(),
-            dtls_transport_->GetMyFingerprint().c_str(),  pt, ssrc, ssrc, ssrc, ssrc, "4", ssrc, ip.c_str(), port, "host");
+            finger_print_sha256.c_str(),  pt, ssrc, ssrc, ssrc, ssrc, "4", ssrc, ip.c_str(), port, "host");
     return sdp;
-}
-
-void WebRtcTransport::OnIceServerCompleted() {
-    InfoL;
-    dtls_transport_->Start();
-    onIceConnected();
-}
-
-void WebRtcTransport::OnDtlsCompleted(std::string client_key, std::string server_key, RTC::CryptoSuite srtp_crypto_suite) {
-    InfoL << client_key << " " << server_key << " " << (int)srtp_crypto_suite;
-    srtp_session_ = std::make_shared<RTC::SrtpSession>(RTC::SrtpSession::Type::OUTBOUND, srtp_crypto_suite, (uint8_t *) client_key.c_str(), client_key.size());
-    onDtlsCompleted();
 }
 
 bool is_dtls(char *buf) {
@@ -90,25 +128,23 @@ bool is_rtcp(char *buf) {
     return ((header->pt >= 64) && (header->pt < 96));
 }
 
-void WebRtcTransport::OnInputDataPacket(char *buf, size_t len, struct sockaddr_in *remote_address) {
+void WebRtcTransport::OnInputDataPacket(char *buf, size_t len, RTC::TransportTuple *tuple) {
     if (RTC::StunPacket::IsStun((const uint8_t *) buf, len)) {
-        InfoL << "stun:" << hexdump(buf, len);
         RTC::StunPacket *packet = RTC::StunPacket::Parse((const uint8_t *) buf, len);
         if (packet == nullptr) {
             WarnL << "parse stun error" << std::endl;
             return;
         }
-        ice_server_->ProcessStunPacket(packet, remote_address);
+        ice_server_->ProcessStunPacket(packet, tuple);
         return;
     }
-    if (DtlsTransport::IsDtlsPacket(buf, len)) {
-        InfoL << "dtls:" << hexdump(buf, len);
-        dtls_transport_->InputData(buf, len);
+    if (is_dtls(buf)) {
+        dtls_transport_->ProcessDtlsData((uint8_t *)buf, len);
         return;
     }
     if (is_rtp(buf)) {
         RtpHeader *header = (RtpHeader *) buf;
-        InfoL << "rtp:" << header->dumpString(len);
+//        InfoL << "rtp:" << header->dumpString(len);
         return;
     }
     if (is_rtcp(buf)) {
@@ -118,10 +154,6 @@ void WebRtcTransport::OnInputDataPacket(char *buf, size_t len, struct sockaddr_i
     }
 }
 
-void WebRtcTransport::WritePacket(char *buf, size_t len, struct sockaddr_in *remote_address) {
-    onWrite(buf, len, remote_address ? remote_address : (ice_server_ ? ice_server_->GetSelectAddr() : nullptr));
-}
-
 void WebRtcTransport::WritRtpPacket(char *buf, size_t len) {
     const uint8_t *p = (uint8_t *) buf;
     bool ret = false;
@@ -129,7 +161,7 @@ void WebRtcTransport::WritRtpPacket(char *buf, size_t len) {
         ret = srtp_session_->EncryptRtp(&p, &len);
     }
     if (ret) {
-        onWrite((char *) p, len, ice_server_->GetSelectAddr());
+        onWrite((char *) p, len);
     }
 }
 
@@ -139,8 +171,8 @@ WebRtcTransportImp::WebRtcTransportImp(const EventPoller::Ptr &poller) {
     _socket = Socket::createSocket(poller, false);
     //随机端口，绑定全部网卡
     _socket->bindUdpSock(0);
-    _socket->setOnRead([this](const Buffer::Ptr &buf, struct sockaddr *addr, int addr_len){
-        OnInputDataPacket(buf->data(), buf->size(), (struct sockaddr_in*)addr);
+    _socket->setOnRead([this](const Buffer::Ptr &buf, struct sockaddr *addr, int addr_len) mutable {
+        OnInputDataPacket(buf->data(), buf->size(), addr);
     });
 }
 
@@ -149,7 +181,7 @@ void WebRtcTransportImp::attach(const RtspMediaSource::Ptr &src) {
     _src = src;
 }
 
-void WebRtcTransportImp::onDtlsCompleted() {
+void WebRtcTransportImp::onDtlsConnected() {
     _reader = _src->getRing()->attach(_socket->getPoller(), true);
     weak_ptr<WebRtcTransportImp> weak_self = shared_from_this();
     _reader->setReadCB([weak_self](const RtspMediaSource::RingDataType &pkt){
@@ -167,14 +199,9 @@ void WebRtcTransportImp::onDtlsCompleted() {
     });
 }
 
-void WebRtcTransportImp::onIceConnected(){
-
-}
-
 void WebRtcTransportImp::onWrite(const char *buf, size_t len, struct sockaddr_in *dst) {
     auto ptr = BufferRaw::create();
     ptr->assign(buf, len);
-//    InfoL << len << " " << SockUtil::inet_ntoa(dst->sin_addr) << " " << ntohs(dst->sin_port);
     _socket->send(ptr, (struct sockaddr *)(dst), sizeof(struct sockaddr));
 }
 
@@ -200,16 +227,6 @@ std::string WebRtcTransportImp::getIP() const {
 }
 
 ///////////////////////////////////////////////////////////////////
-
-INSTANCE_IMP(WebRtcManager)
-
-WebRtcManager::WebRtcManager() {
-
-}
-
-WebRtcManager::~WebRtcManager() {
-
-}
 
 
 
