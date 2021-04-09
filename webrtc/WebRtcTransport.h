5b7974a522abceba6bc7760ea33de3b94ef04434
xiongziliang <771730766@qq.com>
2021-04-09 20:42:36 +0800

    添加版权信息

--- webrtc/WebRtcTransport.h
+++ webrtc/WebRtcTransport.h
@@ -1,4 +1,14 @@
-﻿#pragma once
+﻿/*
+ * Copyright (c) 2016 The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/xia-chu/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#pragma once
 
 #include <memory>
 #include <string>
