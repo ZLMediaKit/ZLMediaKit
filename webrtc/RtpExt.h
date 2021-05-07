2e8a7be5eadf540f49382228399240a43d2fecca
xia-chu <771730766@qq.com>
2021-05-07 12:00:26 +0800

    完善rtp ext相关代码

--- webrtc/RtpExt.h
+++ webrtc/RtpExt.h
@@ -20,9 +20,37 @@
 using namespace std;
 using namespace mediakit;
 
-class RtpExt {
+enum class RtpExtType : uint8_t {
+    padding = 0,
+    ssrc_audio_level = 1,
+    abs_send_time = 2,
+    transport_cc = 3,
+    sdes_mid = 4,
+    sdes_rtp_stream_id = 5,
+    sdes_repaired_rtp_stream_id = 6,
+    video_timing = 7,
+    color_space = 8,
+    video_content_type = 11,
+    playout_delay = 12,
+    video_orientation = 13,
+    toffset = 14,
+    reserved = 15,
+};
+
+class RtcMedia;
+
+class RtpExt : public std::string {
 public:
-    static map<uint8_t/*id*/, string/*data*/> getExtValue(const RtpHeader *header);
+    ~RtpExt() = default;
+    static map<uint8_t/*id*/, RtpExt/*data*/> getExtValue(const RtpHeader *header, const RtcMedia &media);
+    static RtpExtType getRtpExtType(const string &url);
+    static const string& getRtpExtUrl(RtpExtType type);
+
+private:
+    RtpExt(RtpExtType type, const char *str, size_t size) : std::string(str, size), _type(type) {}
+
+private:
+    RtpExtType _type;
 };
 
 
