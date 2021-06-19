89d8f005f8bdad0c74afaa574f15a18272958665
Johnny <hellojinqiang@gmail.com>
2021-06-19 23:56:17 +0800

    fix: fix typo.

--- webrtc/RtpExt.cpp
+++ webrtc/RtpExt.cpp
@@ -390,7 +390,7 @@ string RtpExt::getSdesMid() const {
 
 
 //https://tools.ietf.org/html/draft-ietf-avtext-rid-06
-//用于simulecast
+//用于simulcast
 //3.1.  RTCP 'RtpStreamId' SDES Extension
 //
 //        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
