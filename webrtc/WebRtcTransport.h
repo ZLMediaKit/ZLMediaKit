edf30a04cfe0b4f8c2c23f4d2a21ead1e426804f
ziyue <1213642868@qq.com>
2021-06-25 17:17:48 +0800

    优化代码

--- webrtc/WebRtcTransport.h
+++ webrtc/WebRtcTransport.h
@@ -216,7 +216,7 @@ private:
 
     void onSortedRtp(MediaTrack &track, const string &rid, RtpPacket::Ptr rtp);
     void onSendNack(MediaTrack &track, const FCI_NACK &nack, uint32_t ssrc);
-    void createRtpChannel(const string &rid, uint32_t ssrc, const MediaTrack::Ptr &info);
+    void createRtpChannel(const string &rid, uint32_t ssrc, const MediaTrack::Ptr &track);
 
 private:
     uint16_t _rtx_seq[2] = {0, 0};
