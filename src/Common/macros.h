dfbfd65ab33a1439fcb5496a50a3130e51d306bd
ziyue <1213642868@qq.com>
2021-06-28 20:29:56 +0800

    宏定义移至macros.h文件

--- src/Common/macros.h
+++ src/Common/macros.h
@@ -37,5 +37,38 @@
     #endif //!defined(_WIN32)
 #endif
 
+#ifndef CHECK
+#define CHECK(exp) Assert_Throw(!(exp), #exp, __FUNCTION__, __FILE__, __LINE__)
+#endif//CHECK
+
+#ifndef MAX
+#define MAX(a,b) ((a) > (b) ? (a) : (b) )
+#endif //MAX
+
+#ifndef MIN
+#define MIN(a,b) ((a) < (b) ? (a) : (b) )
+#endif //MIN
+
+#ifndef CLEAR_ARR
+#define CLEAR_ARR(arr) for(auto &item : arr){ item = 0;}
+#endif //CLEAR_ARR
+
+#define SERVER_NAME "ZLMediaKit-5.0(build in " __DATE__ " " __TIME__ ")"
+#define VHOST_KEY "vhost"
+#define HTTP_SCHEMA "http"
+#define RTSP_SCHEMA "rtsp"
+#define RTMP_SCHEMA "rtmp"
+#define HLS_SCHEMA "hls"
+#define TS_SCHEMA "ts"
+#define FMP4_SCHEMA "fmp4"
+#define DEFAULT_VHOST "__defaultVhost__"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void Assert_Throw(int failed, const char *exp, const char *func, const char *file, int line);
+#ifdef __cplusplus
+}
+#endif
 
 #endif //ZLMEDIAKIT_MACROS_H
