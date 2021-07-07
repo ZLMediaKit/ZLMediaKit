ec640a38103b0654524de042be07d7cd6b61f119
ziyue <1213642868@qq.com>
2021-07-07 11:37:22 +0800

    由于frame merge时已经过滤无效帧，所以移除多余代码

--- src/Record/MP4Muxer.cpp
+++ src/Record/MP4Muxer.cpp
@@ -86,13 +86,7 @@ void MP4MuxerInterface::inputFrame(const Frame::Ptr &frame) {
     int64_t dts_out, pts_out;
 
     switch (frame->getCodecId()) {
-        case CodecH264: {
-            int type = H264_TYPE(*((uint8_t *)frame->data() + frame->prefixSize()));
-            if(type == H264Frame::NAL_SEI){
-                break;
-            }
-        }
-
+        case CodecH264:
         case CodecH265: {
             //这里的代码逻辑是让SPS、PPS、IDR这些时间戳相同的帧打包到一起当做一个帧处理，
             _frame_merger.inputFrame(frame, [&](uint32_t dts, uint32_t pts, const Buffer::Ptr &buffer, bool have_idr) {
