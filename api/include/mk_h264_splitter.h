f5842f8c1fcf01f5f1f5ac603721471d44b745dc
ziyue <1213642868@qq.com>
2021-08-02 15:54:51 +0800

    新增读取h264文件转流媒体范例

---
+++ api/include/mk_h264_splitter.h
@@ -0,0 +1,56 @@
+﻿/*
+ * Copyright (c) 2016 The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/xia-chu/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef ZLMEDIAKIT_MK_H264_SPLITTER_H
+#define ZLMEDIAKIT_MK_H264_SPLITTER_H
+
+#include "mk_common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void *mk_h264_splitter;
+
+/**
+ * h264 分帧器输出回调函数
+ * @param user_data 设置回调时的用户数据指针
+ * @param splitter 对象
+ * @param frame 帧数据
+ * @param size 帧数据长度
+ */
+typedef void(API_CALL *on_mk_h264_splitter_frame)(void *user_data, mk_h264_splitter splitter, const char *frame, int size);
+
+/**
+ * 创建h264分帧器
+ * @param cb 分帧回调函数
+ * @param user_data 回调用户数据指针
+ * @return 分帧器对象
+ */
+API_EXPORT mk_h264_splitter API_CALL mk_h264_splitter_create(on_mk_h264_splitter_frame cb, void *user_data);
+
+/**
+ * 删除h264分帧器
+ * @param ctx 分帧器
+ */
+API_EXPORT void API_CALL mk_h264_splitter_release(mk_h264_splitter ctx);
+
+/**
+ * 输入数据并分帧
+ * @param ctx 分帧器
+ * @param data h264/h265数据
+ * @param size 数据长度
+ */
+API_EXPORT void API_CALL mk_h264_splitter_input_data(mk_h264_splitter ctx, const char *data, int size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //ZLMEDIAKIT_MK_H264_SPLITTER_H
---
+++ api/source/mk_h264_splitter.cpp
@@ -0,0 +1,81 @@
+﻿/*
+ * Copyright (c) 2016 The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/xia-chu/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "mk_h264_splitter.h"
+#include "Http/HttpRequestSplitter.h"
+
+using namespace mediakit;
+
+class H264Splitter : public HttpRequestSplitter {
+public:
+    using onH264 = function<void(const char *data, size_t len)>;
+    H264Splitter() = default;
+    ~H264Splitter() override;
+    void setOnSplitted(onH264 cb);
+
+protected:
+    ssize_t onRecvHeader(const char *data, size_t len) override;
+    const char *onSearchPacketTail(const char *data, size_t len) override;
+
+private:
+    onH264 _cb;
+};
+
+void H264Splitter::setOnSplitted(H264Splitter::onH264 cb) {
+    _cb = std::move(cb);
+}
+
+H264Splitter::~H264Splitter() {
+    if (remainDataSize()) {
+        _cb(remainData(), remainDataSize());
+    }
+}
+
+ssize_t H264Splitter::onRecvHeader(const char *data, size_t len) {
+    _cb(data, len);
+    return 0;
+}
+
+const char *H264Splitter::onSearchPacketTail(const char *data, size_t len) {
+    for (size_t i = 2; len > 2 && i < len - 2; ++i) {
+        //判断0x00 00 01
+        if (data[i] == 0 && data[i + 1] == 0 && data[i + 2] == 1) {
+            if (i > 0 && data[i - 1] == 0) {
+                //找到0x00 00 00 01
+                return data + i - 1;
+            }
+            return data + i;
+        }
+    }
+    return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+API_EXPORT mk_h264_splitter API_CALL mk_h264_splitter_create(on_mk_h264_splitter_frame cb, void *user_data) {
+    assert(cb);
+    auto ptr = new H264Splitter();
+    ptr->setOnSplitted([cb, ptr, user_data](const char *data, size_t len) {
+        cb(user_data, reinterpret_cast<mk_h264_splitter>(ptr), data, len);
+    });
+    return reinterpret_cast<mk_h264_splitter>(ptr);
+}
+
+API_EXPORT void API_CALL mk_h264_splitter_release(mk_h264_splitter ctx){
+    assert(ctx);
+    auto ptr = reinterpret_cast<H264Splitter *>(ctx);
+    delete ptr;
+}
+
+API_EXPORT void API_CALL mk_h264_splitter_input_data(mk_h264_splitter ctx, const char *data, int size) {
+    assert(ctx && data && size);
+    auto ptr = reinterpret_cast<H264Splitter *>(ctx);
+    ptr->input(data, size);
+}
