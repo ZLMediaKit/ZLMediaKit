4798b02206e8f88873d142344a6e23a397efa4f6
monktan <monktan@tencent.com>
2021-09-13 21:16:22 +0800

    添加k8s部署相关说明

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,69 @@
 #!/bin/bash
 set -e
-docker build -t gemfield/zlmediakit:20.04-runtime-ubuntu18.04 -f docker/ubuntu18.04/Dockerfile.runtime .
-docker build -t gemfield/zlmediakit:20.04-devel-ubuntu18.04 -f docker/ubuntu18.04/Dockerfile.devel .
-docker build -t gemfield/zlmediakit:20.04-runtime-ubuntu16.04 -f docker/ubuntu16.04/Dockerfile.runtime .
-docker build -t gemfield/zlmediakit:20.04-devel-ubuntu16.04 -f docker/ubuntu16.04/Dockerfile.devel .
-docker build -t gemfield/zlmediakit:centos7-runtime -f docker/centos7/Dockerfile.runtime .
+while getopts c:t:m:v:  opt
+do
+        case $opt in
+                t)
+                        type=$OPTARG
+                        ;;
+	        v)
+			version=$OPTARG
+			;;
+		m)
+			model=$OPTARG
+			;;
+                ?)
+                        echo "unkonwn"
+                        exit
+                        ;;
+       esac
+done
+
+if [[ ! -n $type ]];then
+        echo ".sh [-t build|push] [-m Debug|Release] [-v [version]]"
+        exit
+fi
+
+if [[ ! -n $model ]];then
+        echo ".sh [-t build|push] [-m Debug|Release] [-v [version]]"
+        exit
+fi
+
+if [[ ! -n $version ]];then
+        echo "use latest no version set"
+        version="latest"
+fi
+
+case $model in
+	'Debug')
+		;;
+	'Release')
+		;;
+	*)
+        	echo "unkonwn model"
+		echo ".sh [-t build|push] [-m Debug|Release] [-v [version]]"
+                exit
+                ;;
+esac
+
+namespace="zlmediakit"
+packagename="zlm-mediaserver"
+
+case $type in
+	'build')
+	rm -rf ./build/CMakeCache.txt
+	# 以腾讯云账号为例
+	docker build --build-arg MODEL=$model -t ccr.ccs.tencentyun.com/$namespace/$packagename:$model.$version .
+		;;
+	'push')
+		echo "push to dst registry"
+		# 以腾讯云账号为例
+		docker login --username=default_name ccr.ccs.tencentyun.com
+		docker push ccr.ccs.tencentyun.com/$namespace/$packagename:$model.$version
+		;;
+ 	*)
+                echo "unkonwn type"
+                echo ".sh [-t build|push] [-m Debug|Release] [-v [version]]"
+                exit
+                ;;
+esac
