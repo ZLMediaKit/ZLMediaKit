89ad48fb76a5bf9cfeef3fc410b5523b3a7d1db1
夏楚 <771730766@qq.com>
2021-09-29 16:54:09 +0800

    格式化代码

--- src/Rtmp/RtmpProtocol.cpp
+++ src/Rtmp/RtmpProtocol.cpp
@@ -166,10 +166,10 @@ void RtmpProtocol::sendInvoke(const string &cmd, const AMFValue &val) {
 
 void RtmpProtocol::sendRequest(int cmd, const string& str) {
     // 若 cmd 属于 Protocol Control Messages ，则应使用 chunk id 2 发送
-    if(cmd <= MSG_SET_PEER_BW){
+    if (cmd <= MSG_SET_PEER_BW) {
         sendRtmp(cmd, _stream_index, str, 0, CHUNK_NETWORK);
-    // 否则使用 chunk id 发送(任意值3-128，参见 obs 及 ffmpeg 选取 3)
-    }else{
+        // 否则使用 chunk id 发送(任意值3-128，参见 obs 及 ffmpeg 选取 3)
+    } else {
         sendRtmp(cmd, _stream_index, str, 0, CHUNK_SYSTEM);
     }
 }
