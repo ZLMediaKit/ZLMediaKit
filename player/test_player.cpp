4ef1764c0eb95ca019d594c44a3ab207f4a6da29
ziyue <1213642868@qq.com>
2021-11-10 15:30:31 +0800

    Player: 不等待track ready，加快秒开速度

--- player/test_player.cpp
+++ player/test_player.cpp
@@ -113,6 +113,8 @@ int main(int argc, char *argv[]) {
     });
 
     (*player)[kRtpType] = atoi(argv[2]);
+    //不等待track ready再回调播放成功事件，这样可以加快秒开速度
+    (*player)[Client::kWaitTrackReady] = false;
     player->play(argv[1]);
     SDLDisplayerHelper::Instance().runLoop();
     return 0;
