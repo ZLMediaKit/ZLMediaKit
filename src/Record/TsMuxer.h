44378c83fdae77b72805191dcb601312db721327
ziyue <1213642868@qq.com>
2021-12-28 17:09:20 +0800

    TS: hls/http-ts 改为绝对时间戳

--- src/Record/TsMuxer.h
+++ src/Record/TsMuxer.h
@@ -12,13 +12,16 @@
 #define TSMUXER_H
 
 #if defined(ENABLE_HLS)
+
 #include <unordered_map>
 #include "Extension/Frame.h"
 #include "Extension/Track.h"
 #include "Util/File.h"
 #include "Common/MediaSink.h"
 #include "Common/Stamp.h"
+
 using namespace toolkit;
+
 namespace mediakit {
 
 //该类用于产生MPEG-TS
@@ -49,15 +52,13 @@ protected:
      * @param timestamp 时间戳，单位毫秒
      * @param is_idr_fast_packet 是否为关键帧的第一个TS包，用于确保ts切片第一帧为关键帧
      */
-    virtual void onTs(std::shared_ptr<Buffer> buffer, uint32_t timestamp,bool is_idr_fast_packet) = 0;
+    virtual void onTs(std::shared_ptr<Buffer> buffer, uint32_t timestamp, bool is_idr_fast_packet) = 0;
 
 private:
     void init();
     void uninit();
-    //音视频时间戳同步用
-    void stampSync();
-    void onTs_l(const void *packet, size_t bytes);
     void flushCache();
+    void onTs_l(const void *packet, size_t bytes);
 
 private:
     bool _have_video = false;
@@ -65,11 +66,7 @@ private:
     void *_context = nullptr;
     char _tsbuf[188];
     uint32_t _timestamp = 0;
-    struct track_info {
-        int track_id = -1;
-        Stamp stamp;
-    };
-    unordered_map<int, track_info> _codec_to_trackid;
+    unordered_map<int, int/*track_id*/> _codec_to_trackid;
     FrameMerger _frame_merger{FrameMerger::h264_prefix};
     std::shared_ptr<BufferLikeString> _cache;
 };
