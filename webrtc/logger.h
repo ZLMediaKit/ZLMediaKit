80d24d28842bed828d82eb614de579266859241e
ziyue <1213642868@qq.com>
2021-12-31 14:24:15 +0800

    修复Windows下编译问题

--- webrtc/logger.h
+++ webrtc/logger.h
@@ -4,12 +4,12 @@ using namespace toolkit;
 
 #define MS_TRACE()
 #define MS_ERROR PrintE
-#define MS_THROW_ERROR(fmt, ...) do { PrintE(fmt, ##__VA_ARGS__); throw std::runtime_error("error"); } while(false)
+#define MS_THROW_ERROR(...) do { PrintE(__VA_ARGS__); throw std::runtime_error("error"); } while(false)
 #define MS_DUMP PrintT
-#define MS_DEBUG_2TAGS(tag1, tag2, fmt, ...) PrintD(fmt, ##__VA_ARGS__)
-#define MS_WARN_2TAGS(tag1, tag2, fmt, ...) PrintW(fmt, ##__VA_ARGS__)
-#define MS_DEBUG_TAG(tag, fmt, ...) PrintD(fmt, ##__VA_ARGS__)
-#define MS_ASSERT(con, fmt, ...) do { if(!(con)) { PrintE(fmt, ##__VA_ARGS__); abort(); } } while(false)
-#define MS_ABORT(fmt, ...) do { PrintE(fmt, ##__VA_ARGS__); abort(); } while(false)
-#define MS_WARN_TAG(tag, fmt, ...) PrintW(fmt, ##__VA_ARGS__)
+#define MS_DEBUG_2TAGS(tag1, tag2, ...) PrintD(__VA_ARGS__)
+#define MS_WARN_2TAGS(tag1, tag2, ...) PrintW(__VA_ARGS__)
+#define MS_DEBUG_TAG(tag, ...) PrintD(__VA_ARGS__)
+#define MS_ASSERT(con, ...) do { if(!(con)) { PrintE(__VA_ARGS__); abort(); } } while(false)
+#define MS_ABORT(...) do { PrintE(__VA_ARGS__); abort(); } while(false)
+#define MS_WARN_TAG(tag, ...) PrintW(__VA_ARGS__)
 #define MS_DEBUG_DEV PrintD
