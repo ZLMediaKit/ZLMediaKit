294a3a94b2689593519c9dc58b2a51f9327079db
ziyue <1213642868@qq.com>
2022-01-14 13:09:47 +0800

    修复丢包导致aac确实导致内存越界的bug: #1363

--- src/Extension/AAC.cpp
+++ src/Extension/AAC.cpp
@@ -279,6 +279,11 @@ bool AACTrack::inputFrame(const Frame::Ptr &frame) {
         }
         auto sub_frame = std::make_shared<FrameInternal<FrameFromPtr> >(frame, (char *) ptr, frame_len, ADTS_HEADER_LEN);
         ptr += frame_len;
+        if (ptr > end) {
+            WarnL << "invalid aac length in adts header: " << frame_len
+                  << ", remain data size: " << end - (ptr - frame_len);
+            break;
+        }
         sub_frame->setCodecId(CodecAAC);
         if (inputFrame_l(sub_frame)) {
             ret = true;
