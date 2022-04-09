471a8b77355a560c1ba2fe7fa6d8e879f3456558
alexliyu7352 <liyu7352@gmail.com>
2022-04-09 20:37:36 +0800

    子进程重启失败时，不断重试，避免重启失败 (#1545)
    * Update main.cpp
    
    * 双重保险, 避免重启失败
    
    业务繁忙的服务器当子进程崩溃后,
    虽然延时了3秒, 但是如果服务器负载很高, 或者开启了coredump
    会导致拉起子进程时, 仍旧端口占用导致子进程重启失败而直接退出主进程.
    
    因此, 这里做了双重保险, 当子进程拉起时如果端口占用, 那么子进程会继续重试.

--- server/main.cpp
+++ server/main.cpp
@@ -318,7 +318,7 @@ int start_main(int argc,char *argv[]) {
             sleep(1);
 #if !defined(_WIN32)
             if (pid != getpid()) {
-                kill(pid, SIGINT);
+                kill(pid, SIGUSR1);
             }
 #endif
             return -1;
