d11dfb907c0277d7fa520f50a0ecccfb74ca114c
xiongziliang <771730766@qq.com>
2022-06-11 14:18:55 +0800

    优化录制相关代码

--- api/source/mk_recorder.cpp
+++ api/source/mk_recorder.cpp
@@ -11,6 +11,8 @@
 #include "mk_recorder.h"
 #include "Rtmp/FlvMuxer.h"
 #include "Record/Recorder.h"
+
+using namespace std;
 using namespace toolkit;
 using namespace mediakit;
 
@@ -36,17 +38,43 @@ API_EXPORT int API_CALL mk_flv_recorder_start(mk_flv_recorder ctx, const char *v
 }
 
 ///////////////////////////////////////////hls/mp4录制/////////////////////////////////////////////
+
+static inline bool isRecording(Recorder::type type, const string &vhost, const string &app, const string &stream_id){
+    auto src = MediaSource::find(vhost, app, stream_id);
+    if(!src){
+        return false;
+    }
+    return src->isRecording(type);
+}
+
+static inline  bool startRecord(Recorder::type type, const string &vhost, const string &app, const string &stream_id,const string &customized_path, size_t max_second){
+    auto src = MediaSource::find(vhost, app, stream_id);
+    if (!src) {
+        WarnL << "未找到相关的MediaSource,startRecord失败:" << vhost << "/" << app << "/" << stream_id;
+        return false;
+    }
+    return src->setupRecord(type, true, customized_path, max_second);
+}
+
+static inline bool stopRecord(Recorder::type type, const string &vhost, const string &app, const string &stream_id){
+    auto src = MediaSource::find(vhost, app, stream_id);
+    if(!src){
+        return false;
+    }
+    return src->setupRecord(type, false, "", 0);
+}
+
 API_EXPORT int API_CALL mk_recorder_is_recording(int type, const char *vhost, const char *app, const char *stream){
     assert(vhost && app && stream);
-    return Recorder::isRecording((Recorder::type)type,vhost,app,stream);
+    return isRecording((Recorder::type)type,vhost,app,stream);
 }
 
 API_EXPORT int API_CALL mk_recorder_start(int type, const char *vhost, const char *app, const char *stream,const char *customized_path, size_t max_second){
     assert(vhost && app && stream);
-    return Recorder::startRecord((Recorder::type)type,vhost,app,stream,customized_path ? customized_path : "", max_second);
+    return startRecord((Recorder::type)type,vhost,app,stream,customized_path ? customized_path : "", max_second);
 }
 
 API_EXPORT int API_CALL mk_recorder_stop(int type, const char *vhost, const char *app, const char *stream){
     assert(vhost && app && stream);
-    return Recorder::stopRecord((Recorder::type)type,vhost,app,stream);
+    return stopRecord((Recorder::type)type,vhost,app,stream);
 }
