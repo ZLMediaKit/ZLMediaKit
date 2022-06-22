a3efa0d2c3c2bb456d040b106fe3949f9424871c
baiyfcu <baiyfcu@gmail.com>
2022-06-22 10:31:53 +0800

    封装rtp server创建和关闭

--- server/WebApi.h
+++ server/WebApi.h
@@ -230,6 +230,9 @@ bool checkArgs(Args &args, const First &first, const KeyTypes &...keys) {
 
 void installWebApi();
 void unInstallWebApi();
+
+uint16_t openRtpServer(uint16_t local_port, const std::string &stream_id, bool enable_tcp, const std::string &local_ip, bool re_use_port, uint32_t ssrc);
+bool closeRtpServer(const std::string &stream_id);
 Json::Value makeMediaSourceJson(mediakit::MediaSource &media);
 void getStatisticJson(const std::function<void(Json::Value &val)> &cb);
 void addStreamProxy(const std::string &vhost, const std::string &app, const std::string &stream, const std::string &url, int retry_count,
