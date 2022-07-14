cffc0743d72b6fea17e507e19fd237e2a1cf6f4c
xiongguangjie <xiong_panda@163.com>
2022-07-14 22:49:31 +0800

    format code use clang-format

--- src/Rtcp/RtcpFCI.h
+++ src/Rtcp/RtcpFCI.h
@@ -11,37 +11,37 @@
 #ifndef ZLMEDIAKIT_RTCPFCI_H
 #define ZLMEDIAKIT_RTCPFCI_H
 
-#include "Rtcp.h"
 #include "Common/config.h"
+#include "Rtcp.h"
 
 namespace mediakit {
 
 /////////////////////////////////////////// PSFB ////////////////////////////////////////////////////
 
-//PSFB fmt = 2
-//https://tools.ietf.org/html/rfc4585#section-6.3.2.2
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |            First        |        Number           | PictureID |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//First: 13 bits
-//      The macroblock (MB) address of the first lost macroblock.  The MB
-//      numbering is done such that the macroblock in the upper left
-//      corner of the picture is considered macroblock number 1 and the
-//      number for each macroblock increases from left to right and then
-//      from top to bottom in raster-scan order (such that if there is a
-//      total of N macroblocks in a picture, the bottom right macroblock
-//      is considered macroblock number N).
+// PSFB fmt = 2
+// https://tools.ietf.org/html/rfc4585#section-6.3.2.2
+//     0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |            First        |        Number           | PictureID |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// First: 13 bits
+//       The macroblock (MB) address of the first lost macroblock.  The MB
+//       numbering is done such that the macroblock in the upper left
+//       corner of the picture is considered macroblock number 1 and the
+//       number for each macroblock increases from left to right and then
+//       from top to bottom in raster-scan order (such that if there is a
+//       total of N macroblocks in a picture, the bottom right macroblock
+//       is considered macroblock number N).
 //
-//   Number: 13 bits
-//      The number of lost macroblocks, in scan order as discussed above.
+//    Number: 13 bits
+//       The number of lost macroblocks, in scan order as discussed above.
 //
-//   PictureID: 6 bits
-//      The six least significant bits of the codec-specific identifier
-//      that is used to reference the picture in which the loss of the
-//      macroblock(s) has occurred.  For many video codecs, the PictureID
-//      is identical to the Temporal Reference.
+//    PictureID: 6 bits
+//       The six least significant bits of the codec-specific identifier
+//       that is used to reference the picture in which the loss of the
+//       macroblock(s) has occurred.  For many video codecs, the PictureID
+//       is identical to the Temporal Reference.
 class FCI_SLI {
 public:
     static size_t constexpr kSize = 4;
@@ -101,15 +101,15 @@ public:
 } PACKED;
 #endif
 
-//PSFB fmt = 4
-//https://tools.ietf.org/html/rfc5104#section-4.3.1.1
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |                              SSRC                             |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   | Seq nr.       |    Reserved                                   |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// PSFB fmt = 4
+// https://tools.ietf.org/html/rfc5104#section-4.3.1.1
+//     0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |                              SSRC                             |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    | Seq nr.       |    Reserved                                   |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 class FCI_FIR {
 public:
     static size_t constexpr kSize = 8;
@@ -188,34 +188,34 @@ private:
 
 #endif
 
-//PSFB fmt = 15
-//https://tools.ietf.org/html/draft-alvestrand-rmcat-remb-03
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |  Unique identifier 'R' 'E' 'M' 'B'                            |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |  Num SSRC     | BR Exp    |  BR Mantissa                      |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |   SSRC feedback                                               |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |  ...                                                          |
-// Num SSRC (8 bits):  Number of SSRCs in this message.
+// PSFB fmt = 15
+// https://tools.ietf.org/html/draft-alvestrand-rmcat-remb-03
+//     0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |  Unique identifier 'R' 'E' 'M' 'B'                            |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |  Num SSRC     | BR Exp    |  BR Mantissa                      |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |   SSRC feedback                                               |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |  ...                                                          |
+//  Num SSRC (8 bits):  Number of SSRCs in this message.
 //
-//   BR Exp (6 bits):   The exponential scaling of the mantissa for the
-//               maximum total media bit rate value, ignoring all packet
-//               overhead.  The value is an unsigned integer [0..63], as
-//               in RFC 5104 section 4.2.2.1.
+//    BR Exp (6 bits):   The exponential scaling of the mantissa for the
+//                maximum total media bit rate value, ignoring all packet
+//                overhead.  The value is an unsigned integer [0..63], as
+//                in RFC 5104 section 4.2.2.1.
 //
-//   BR Mantissa (18 bits):   The mantissa of the maximum total media bit
-//               rate (ignoring all packet overhead) that the sender of
-//               the REMB estimates.  The BR is the estimate of the
-//               traveled path for the SSRCs reported in this message.
-//               The value is an unsigned integer in number of bits per
-//               second.
+//    BR Mantissa (18 bits):   The mantissa of the maximum total media bit
+//                rate (ignoring all packet overhead) that the sender of
+//                the REMB estimates.  The BR is the estimate of the
+//                traveled path for the SSRCs reported in this message.
+//                The value is an unsigned integer in number of bits per
+//                second.
 //
-//   SSRC feedback (32 bits)  Consists of one or more SSRC entries which
-//               this feedback message applies to.
+//    SSRC feedback (32 bits)  Consists of one or more SSRC entries which
+//                this feedback message applies to.
 class FCI_REMB {
 public:
     static size_t constexpr kSize = 8;
@@ -227,9 +227,9 @@ public:
     std::vector<uint32_t> getSSRC();
 
 private:
-    //Unique identifier 'R' 'E' 'M' 'B'
+    // Unique identifier 'R' 'E' 'M' 'B'
     char magic[4];
-    //Num SSRC (8 bits)/BR Exp (6 bits)/ BR Mantissa (18 bits)
+    // Num SSRC (8 bits)/BR Exp (6 bits)/ BR Mantissa (18 bits)
     uint8_t bitrate[4];
     // SSRC feedback (32 bits)  Consists of one or more SSRC entries which
     //               this feedback message applies to.
@@ -238,13 +238,13 @@ private:
 
 /////////////////////////////////////////// RTPFB ////////////////////////////////////////////////////
 
-//RTPFB fmt = 1
-//https://tools.ietf.org/html/rfc4585#section-6.2.1
-// 0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |            PID                |             BLP               |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// RTPFB fmt = 1
+// https://tools.ietf.org/html/rfc4585#section-6.2.1
+//  0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |            PID                |             BLP               |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 class FCI_NACK {
 public:
     static constexpr size_t kSize = 4;
@@ -255,8 +255,8 @@ public:
     void check(size_t size);
     uint16_t getPid() const;
     uint16_t getBlp() const;
-    //返回丢包列表，总长度17，第一个包必丢
-    // TODO: replace std::bitset
+    // 返回丢包列表，总长度17，第一个包必丢
+    //  TODO: replace std::bitset
     std::vector<bool> getBitArray() const;
     std::string dumpString() const;
 
@@ -318,47 +318,48 @@ public:
 } PACKED;
 #endif
 
-enum class SymbolStatus : uint8_t{
-    //Packet not received
+enum class SymbolStatus : uint8_t {
+    // Packet not received
     not_received = 0,
-    //Packet received, small delta （所谓small detal是指能用一个字节表示的数值）
+    // Packet received, small delta （所谓small detal是指能用一个字节表示的数值）
     small_delta = 1,
     // Packet received, large ornegative delta （large即是能用两个字节表示的数值）
     large_delta = 2,
-    //Reserved
+    // Reserved
     reserved = 3
 };
 
-//RTPFB fmt = 15
-//https://tools.ietf.org/html/draft-holmer-rmcat-transport-wide-cc-extensions-01#section-3.1
-//https://zhuanlan.zhihu.com/p/206656654
-//   0                   1                   2                   3
-//   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  |      base sequence number     |      packet status count      |
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  |                 reference time                | fb pkt. count |
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  |          packet chunk         |         packet chunk          |
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  .                                                               .
-//  .                                                               .
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  |         packet chunk          |  recv delta   |  recv delta   |
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  .                                                               .
-//  .                                                               .
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//  |           recv delta          |  recv delta   | zero padding  |
-//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-class FCI_TWCC{
+// RTPFB fmt = 15
+// https://tools.ietf.org/html/draft-holmer-rmcat-transport-wide-cc-extensions-01#section-3.1
+// https://zhuanlan.zhihu.com/p/206656654
+//    0                   1                   2                   3
+//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |      base sequence number     |      packet status count      |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |                 reference time                | fb pkt. count |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |          packet chunk         |         packet chunk          |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   .                                                               .
+//   .                                                               .
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |         packet chunk          |  recv delta   |  recv delta   |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   .                                                               .
+//   .                                                               .
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//   |           recv delta          |  recv delta   | zero padding  |
+//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+class FCI_TWCC {
 public:
     static size_t constexpr kSize = 8;
-    using TwccPacketStatus = std::map<uint16_t/*rtp ext seq*/, std::pair<SymbolStatus, int16_t/*recv delta,单位为250us*/> >;
+    using TwccPacketStatus
+        = std::map<uint16_t /*rtp ext seq*/, std::pair<SymbolStatus, int16_t /*recv delta,单位为250us*/>>;
     void check(size_t size);
     std::string dumpString(size_t total_size) const;
     uint16_t getBaseSeq() const;
-    //单位64ms
+    // 单位64ms
     uint32_t getReferenceTime() const;
     uint16_t getPacketCount() const;
     TwccPacketStatus getPacketChunkList(size_t total_size) const;
@@ -366,15 +367,15 @@ public:
     static std::string create(uint32_t ref_time, uint8_t fb_pkt_count, TwccPacketStatus &status);
 
 private:
-    //base sequence number,基础序号,本次反馈的第一个包的序号;也就是RTP扩展头的序列号
+    // base sequence number,基础序号,本次反馈的第一个包的序号;也就是RTP扩展头的序列号
     uint16_t base_seq;
-    //packet status count, 包个数,本次反馈包含多少个包的状态;从基础序号开始算
+    // packet status count, 包个数,本次反馈包含多少个包的状态;从基础序号开始算
     uint16_t pkt_status_count;
-    //reference time,基准时间,绝对时间;计算该包中每个媒体包的到达时间都要基于这个基准时间计算
+    // reference time,基准时间,绝对时间;计算该包中每个媒体包的到达时间都要基于这个基准时间计算
     uint8_t ref_time[3];
-    //feedback packet count,反馈包号,本包是第几个transport-cc包，每次加1                          |
+    // feedback packet count,反馈包号,本包是第几个transport-cc包，每次加1                          |
     uint8_t fb_pkt_count;
 } PACKED;
 
-} //namespace mediakit
-#endif //ZLMEDIAKIT_RTCPFCI_H
+} // namespace mediakit
+#endif // ZLMEDIAKIT_RTCPFCI_H
