f1bc4665f78b28de9d914907906b80898a59da02
夏楚 <771730766@qq.com>
2022-08-03 21:00:33 +0800

    Update WebApi.cpp

--- server/WebApi.cpp
+++ server/WebApi.cpp
@@ -13,7 +13,6 @@
 #include <signal.h>
 #include <functional>
 #include <unordered_map>
-#include <regex>
 #include "Util/util.h"
 #include "Util/logger.h"
 #include "Util/onceToken.h"
@@ -1334,27 +1333,18 @@ void installWebApi() {
         });
     });
 	
-	// 删除录像文件夹
-	// http://127.0.0.1/index/api/deleteRecordDirectroy?vhost=__defaultVhost__&app=live&stream=ss&period=2020-01-01
+    // 删除录像文件夹
+    // http://127.0.0.1/index/api/deleteRecordDirectroy?vhost=__defaultVhost__&app=live&stream=ss&period=2020-01-01
     api_regist("/index/api/deleteRecordDirectroy", [](API_ARGS_MAP) {
         CHECK_SECRET();
         CHECK_ARGS("vhost", "app", "stream");
-        auto record_path = Recorder::getRecordPath(
-            Recorder::type_mp4, allArgs["vhost"], allArgs["app"], allArgs["stream"], allArgs["customized_path"]);
+        auto record_path = Recorder::getRecordPath(Recorder::type_mp4, allArgs["vhost"], allArgs["app"], allArgs["stream"], allArgs["customized_path"]);
         auto period = allArgs["period"];
-        int result = -1;
-
-        //判断传入的是不是路径格式
-        regex  pattern("^((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))$");
-        if (regex_match(period, pattern) ) {
-            record_path = record_path + period + "/";
-            result = File::delete_file(record_path.data());
-			if(!result)
-				record_path = "delete error";
-        } else {
-            record_path = "format error(\"2020-01-01\")";
-        }     
-        
+        record_path = record_path + period + "/";
+        int result = File::delete_file(record_path.data());
+	if(!result) {
+	    record_path = "delete error";
+	}
         val["code"] = result;
         val["path"] = record_path.data();
     });
