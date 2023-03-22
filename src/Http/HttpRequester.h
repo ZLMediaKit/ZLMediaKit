ea45dd3a170b60c809f182f5ed811dbfa53cae00
cqm <mtdxc@126.com>
2023-03-22 21:57:51 +0800

    HttpRequester add retry support

--- src/Http/HttpRequester.h
+++ src/Http/HttpRequester.h
@@ -25,6 +25,10 @@ public:
 
     void setOnResult(const HttpRequesterResult &onResult);
     void startRequester(const std::string &url, const HttpRequesterResult &on_result, float timeout_sec = 10);
+    void setRetry(int count, int delay);
+    int getRetry() const { return _retry; }
+    int getRetryDelay() const { return _retry_delay; }
+    int getMaxRetry() const { return _max_retry; }
     void clear() override;
 
 private:
@@ -33,6 +37,9 @@ private:
     void onResponseCompleted(const toolkit::SockException &ex) override;
 
 private:
+    int _retry = 0;
+    int _max_retry = 0;
+    int _retry_delay = 2000; // ms
     std::string _res_body;
     HttpRequesterResult _on_result;
 };
