d5bf99a27b030eef86c0d7a6e0a928443fb7f72c
xiongguangjie <xiong_panda@163.com>
2023-06-28 14:56:06 +0800

    ts or ps muxer not support mjpeg video codec ignore it (#2593)

--- src/Extension/Frame.h
+++ src/Extension/Frame.h
@@ -40,7 +40,7 @@ typedef enum {
     XX(CodecVP8,   TrackVideo, 7, "VP8", PSI_STREAM_VP8)            \
     XX(CodecVP9,   TrackVideo, 8, "VP9", PSI_STREAM_VP9)            \
     XX(CodecAV1,   TrackVideo, 9, "AV1", PSI_STREAM_AV1)            \
-    XX(CodecJPEG,  TrackVideo, 10, "JPEG", PSI_STREAM_JPEG_2000)
+    XX(CodecJPEG,  TrackVideo, 10, "JPEG", PSI_STREAM_RESERVED)
 
 typedef enum {
     CodecInvalid = -1,
