fd689304e4db117b458715bffa9ca033bbc4c338
xiongguangjie <xiong_panda@163.com>
2023-07-20 11:02:50 +0800

    flv player avoid script data first element not string like livegbs

--- src/Rtmp/FlvSplitter.cpp
+++ src/Rtmp/FlvSplitter.cpp
@@ -89,23 +89,31 @@ void FlvSplitter::onRecvContent(const char *data, size_t len) {
 
         case MSG_DATA:
         case MSG_DATA3: {
+            //TraceL << hexdump(data,len);
             BufferLikeString buffer(string(data, len));
             AMFDecoder dec(buffer, _type == MSG_DATA3 ? 3 : 0);
-            std::string type = dec.load<std::string>();
+            auto first = dec.load<AMFValue>();
+            std::string type = "";
             bool flag = true;
-            if (type == "@setDataFrame") {
-                std::string type = dec.load<std::string>();
-                if (type == "onMetaData") {
+
+            if (first.type() == AMFType::AMF_STRING) {
+                type = first.as_string();
+                if (type == "@setDataFrame") {
+                    std::string type = dec.load<std::string>();
+                    if (type == "onMetaData") {
+                        flag = onRecvMetadata(dec.load<AMFValue>());
+                    } else {
+                        WarnL << "unknown type:" << type;
+                    }
+                } else if (type == "onMetaData") {
                     flag = onRecvMetadata(dec.load<AMFValue>());
                 } else {
-                    WarnL << "unknown type:" << type;
+                    WarnL << "unknown notify:" << type;
                 }
-            } else if (type == "onMetaData") {
-                flag = onRecvMetadata(dec.load<AMFValue>());
             } else {
-                WarnL << "unknown notify:" << type;
+                WarnL << "first type is " << first.type();
             }
-            if(!flag){
+            if (!flag) {
                 throw std::invalid_argument("check rtmp metadata failed");
             }
             return;
