435342762c000e22fec44cf9fc38fad12557711d
xia-chu <771730766@qq.com>
2023-09-23 20:34:36 +0800

    修复ffmpeg日志保存目录相关bug
    每次调用addFFmpegSource/getSnap接口时，都可能导致篡改全局配置变量

--- server/Process.h
+++ server/Process.h
@@ -26,7 +26,7 @@ class Process {
 public:
     Process();
     ~Process();
-    void run(const std::string &cmd, std::string &log_file);
+    void run(const std::string &cmd, std::string log_file);
     void kill(int max_delay,bool force = false);
     bool wait(bool block = true);
     int exit_code();
