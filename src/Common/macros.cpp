2ac54248f439ebef2ff6d3b366054ce1e172d675
xia-chu <771730766@qq.com>
2023-12-09 14:51:22 +0800

    release 8.0

--- src/Common/macros.cpp
+++ src/Common/macros.cpp
@@ -35,7 +35,7 @@ namespace mediakit {
 
 //请遵循MIT协议，勿修改服务器声明
 #if !defined(ENABLE_VERSION)
-const char kServerName[] =  "ZLMediaKit-7.0(build in " __DATE__ " " __TIME__ ")";
+const char kServerName[] =  "ZLMediaKit-8.0(build in " __DATE__ " " __TIME__ ")";
 #else
 const char kServerName[] = "ZLMediaKit(git hash:" COMMIT_HASH "/" COMMIT_TIME ",branch:" BRANCH_NAME ",build time:" BUILD_TIME ")";
 #endif
