c1f75bb1024a30ced95081d2e125885e54da1301
PioLing <964472638@qq.com>
2023-12-11 16:24:54 +0800

    修正 getCodecU 返回值  (#3104)
    'g711 getCodecU() 返回值改为 CodecG711U'

--- ext-codec/G711.cpp
+++ ext-codec/G711.cpp
@@ -79,7 +79,7 @@ CodecId getCodecA() {
 }
 
 CodecId getCodecU() {
-    return CodecG711A;
+    return CodecG711U;
 }
 
 Track::Ptr getTrackByCodecId_l(CodecId codec, int sample_rate, int channels, int sample_bit) {
