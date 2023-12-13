152d36f8ed7fb27314665c3c9604fb6ce27446c6
Waken <c373906408@live.cn>
2023-12-13 17:07:02 +0800

    修复一处frame为空导致的崩溃

--- src/Rtp/Decoder.cpp
+++ src/Rtp/Decoder.cpp
@@ -115,9 +115,11 @@ void DecoderImp::onDecode(int stream, int codecid, int flags, int64_t pts, int64
         onFrame(stream, frame);
         return;
     }
-    ref.second.inputFrame(frame, [&,codec](uint64_t dts, uint64_t pts, const Buffer::Ptr &buffer, bool) {
-        onFrame(stream, Factory::getFrameFromBuffer(codec, buffer, dts, pts));
-    });
+    if(ref.first){
+        ref.second.inputFrame(frame, [&](uint64_t dts, uint64_t pts, const Buffer::Ptr &buffer, bool) {
+            onFrame(stream, Factory::getFrameFromBuffer(codec, buffer, dts, pts));
+        });
+    }
 }
 #else
 void DecoderImp::onDecode(int stream,int codecid,int flags,int64_t pts,int64_t dts,const void *data,size_t bytes) {}
