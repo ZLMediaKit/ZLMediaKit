8f03105baae7489874820ba2f527913707ed6d75
xiongguangjie <xiong_panda@163.com>
2024-09-17 09:44:22 +0800

    Fix bug #3894 of rtsp over http (#3903)

--- src/Rtsp/RtspSession.cpp
+++ src/Rtsp/RtspSession.cpp
@@ -128,7 +128,7 @@ void RtspSession::onRecv(const Buffer::Ptr &buf) {
 void RtspSession::onWholeRtspPacket(Parser &parser) {
     string method = parser.method(); //提取出请求命令字
     _cseq = atoi(parser["CSeq"].data());
-    if (_content_base.empty() && method != "GET") {
+    if (_content_base.empty() && method != "GET" && method != "POST" ) {
         RtspUrl rtsp;
         rtsp.parse(parser.url());
         _content_base = rtsp._url;
