4cd6e0ef782bbe1cf7c95654c5bb0371f86998c1
Pioling <964472638@qq.com>
2025-05-27 14:35:34 +0800

    Fixed the linux code.

--- server/main.cpp
+++ server/main.cpp
@@ -257,11 +257,11 @@ int start_main(int argc,char *argv[]) {
             // Start daemon process
             System::startDaemon(kill_parent_if_failed);
         }
-#else
+#endif //! defined(_WIN32)
+
         // 开启崩溃捕获等  [AUTO-TRANSLATED:9c7c759c]
         // Enable crash capture, etc.
         System::systemSetup();
-#endif//!defined(_WIN32)
 
         // 启动异步日志线程  [AUTO-TRANSLATED:c93cc6f4]
         // Start asynchronous log thread
