a35444f27e3c27be68f69f0a7dd6e79e3a1ab6da
xia-chu <771730766@qq.com>
2025-06-29 18:00:47 +0800

    优化SEI/AUD帧处理逻辑

--- ext-codec/H264.cpp
+++ ext-codec/H264.cpp
@@ -153,7 +153,11 @@ bool H264Track::ready() const {
 bool H264Track::inputFrame(const Frame::Ptr &frame) {
     using H264FrameInternal = FrameInternal<H264FrameNoCacheAble>;
     int type = H264_TYPE(frame->data()[frame->prefixSize()]);
-   
+    if (type == H264Frame::NAL_AUD) {
+        // AUD帧丢弃
+        return false;
+    }
+
     if ((type == H264Frame::NAL_B_P || type == H264Frame::NAL_IDR) && ready()) {
         return inputFrame_l(frame);
     }
