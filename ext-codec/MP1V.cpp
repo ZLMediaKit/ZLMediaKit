6e88354b2b45f7249c1d2b9a16705feeafe0c17f
Alex <liyu7352@gmail.com>
2026-07-30 05:58:08 -0700

    遗漏文件了

---
+++ ext-codec/MP1V.cpp
@@ -0,0 +1,96 @@
+﻿/*
+ * Copyright (c) 2016-present The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/ZLMediaKit/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT-like license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include "MP1V.h"
+#include "MP2VRtp.h"
+#include "Extension/Factory.h"
+
+using namespace std;
+using namespace toolkit;
+
+namespace mediakit {
+
+bool MP1VTrack::inputFrame(const Frame::Ptr &frame) {
+    if (!frame) {
+        return false;
+    }
+    auto size = frame->size();
+    auto prefix_size = frame->prefixSize();
+    auto data = frame->data();
+    if (!data || prefix_size > size) {
+        return false;
+    }
+    if (!_sequence_parser.parsed()
+        && _sequence_parser.input((const uint8_t *)data + prefix_size, size - prefix_size)) {
+        _width = _sequence_parser.width();
+        _height = _sequence_parser.height();
+        _fps = _sequence_parser.fps();
+    }
+    return VideoTrackImp::inputFrame(frame);
+}
+
+Sdp::Ptr MP1VTrack::getSdp(uint8_t pt) const {
+    // 当前只接通 TS/PS；静态 RTP PT 32 仍由 CodecMP2V 表示，不能在此伪装成完整 RTSP 支持。
+    // Only TS/PS is wired for now; static RTP PT 32 still maps to CodecMP2V.
+    return nullptr;
+}
+
+namespace {
+
+CodecId getCodec() {
+    return CodecMP1V;
+}
+
+Track::Ptr getTrackByCodecId(int sample_rate, int channels, int sample_bit) {
+    return std::make_shared<MP1VTrack>();
+}
+
+Track::Ptr getTrackBySdp(const SdpTrack::Ptr &track) {
+    WarnL << "Unsupported MP1V sdp track";
+    return nullptr;
+}
+
+RtpCodec::Ptr getRtpEncoderByCodecId(uint8_t pt) {
+    // RFC 2250 的 MPEG Video packetizer 同时适用于 MPEG-1/2。
+    // The RFC 2250 MPEG Video packetizer is shared by MPEG-1 and MPEG-2.
+    return std::make_shared<MP2VRtpEncoder>();
+}
+
+RtpCodec::Ptr getRtpDecoderByCodecId() {
+    WarnL << "Unsupported MP1V rtp decoder";
+    return nullptr;
+}
+
+RtmpCodec::Ptr getRtmpEncoderByTrack(const Track::Ptr &track) {
+    WarnL << "Unsupported MP1V rtmp encoder";
+    return nullptr;
+}
+
+RtmpCodec::Ptr getRtmpDecoderByTrack(const Track::Ptr &track) {
+    WarnL << "Unsupported MP1V rtmp decoder";
+    return nullptr;
+}
+
+Frame::Ptr getFrameFromPtr(const char *data, size_t bytes, uint64_t dts, uint64_t pts) {
+    return std::make_shared<MP1VFrameNoCacheAble>((char *)data, bytes, dts, pts, 0);
+}
+
+} // namespace
+
+CodecPlugin mp1v_plugin = { getCodec,
+                             getTrackByCodecId,
+                             getTrackBySdp,
+                             getRtpEncoderByCodecId,
+                             getRtpDecoderByCodecId,
+                             getRtmpEncoderByTrack,
+                             getRtmpDecoderByTrack,
+                             getFrameFromPtr };
+
+} // namespace mediakit
---
+++ ext-codec/MP1V.h
@@ -0,0 +1,93 @@
+﻿/*
+ * Copyright (c) 2016-present The ZLMediaKit project authors. All Rights Reserved.
+ *
+ * This file is part of ZLMediaKit(https://github.com/ZLMediaKit/ZLMediaKit).
+ *
+ * Use of this source code is governed by MIT-like license that can be found in the
+ * LICENSE file in the root of the source tree. All contributing project authors
+ * may be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef ZLMEDIAKIT_MP1V_H
+#define ZLMEDIAKIT_MP1V_H
+
+#include "Extension/Frame.h"
+#include "Extension/Track.h"
+#include "MpegVideoSequence.h"
+
+namespace mediakit {
+
+/**
+ * MPEG-1 Video 帧辅助类模板
+ * MPEG-1 Video frame helper class template
+ */
+template <typename Parent>
+class MP1VFrameHelper : public Parent {
+public:
+    using Ptr = std::shared_ptr<MP1VFrameHelper>;
+
+    template <typename... ARGS>
+    MP1VFrameHelper(ARGS &&...args)
+        : Parent(std::forward<ARGS>(args)...) {
+        this->_codec_id = CodecMP1V;
+    }
+
+    /**
+     * MPEG-1 picture start code 为 00 00 01 00，picture_coding_type 为 1 时是 I-Picture。
+     * MPEG-1 uses 00 00 01 00 as picture start code; picture_coding_type 1 denotes an I-Picture.
+     */
+    bool keyFrame() const override {
+        auto size = this->size();
+        auto prefix_size = this->prefixSize();
+        auto data = this->data();
+        if (!data || prefix_size > size) {
+            return false;
+        }
+        return isMP1VKeyFrame((const uint8_t *)data + prefix_size, size - prefix_size);
+    }
+
+    bool configFrame() const override { return false; }
+
+    static bool isMP1VKeyFrame(const uint8_t *data, size_t size) {
+        if (!data) {
+            return false;
+        }
+        // 查找 picture start code，再读取 picture_coding_type；循环条件保证可读取到 i + 5。
+        // Locate the picture start code and read picture_coding_type; the loop bounds protect i + 5.
+        for (size_t i = 0; i + 5 < size; ++i) {
+            if (data[i] == 0x00 && data[i + 1] == 0x00 && data[i + 2] == 0x01 && data[i + 3] == 0x00) {
+                auto picture_coding_type = (data[i + 5] >> 3) & 0x07;
+                return picture_coding_type == 1;
+            }
+        }
+        return false;
+    }
+};
+
+using MP1VFrame = MP1VFrameHelper<FrameImp>;
+using MP1VFrameNoCacheAble = MP1VFrameHelper<FrameFromPtr>;
+
+/**
+ * MPEG-1 Video Track
+ */
+class MP1VTrack : public VideoTrackImp {
+public:
+    using Ptr = std::shared_ptr<MP1VTrack>;
+
+    MP1VTrack()
+        : VideoTrackImp(CodecMP1V, 0, 0, 0) {}
+
+    Track::Ptr clone() const override { return std::make_shared<MP1VTrack>(*this); }
+
+    bool inputFrame(const Frame::Ptr &frame) override;
+
+private:
+    Sdp::Ptr getSdp(uint8_t payload_type) const override;
+
+private:
+    MpegVideoSequenceParser _sequence_parser;
+};
+
+} // namespace mediakit
+
+#endif // ZLMEDIAKIT_MP1V_H
